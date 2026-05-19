5bd64f7a773a3fcb643e86627c15e9634546726b
TornadoP <mathbssilva@gmail.com>
2026-05-18 22:17:49 -0300

    criando  uma div

--- app/rotas/obras/[id]/page.tsx
+++ app/rotas/obras/[id]/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
+type Obra = {
+  id: number;
+  nome: string;
+  endereço: string;
+  investimento: string;
+  inicio: string;
+  conclusão: string;
+  descricao: string;
+  progresso: number;
+  imagem: string;
+};
 export default function NomeDaPagina() {
   return (
     <main>
