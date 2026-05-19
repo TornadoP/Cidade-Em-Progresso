8daffe7e0305014c2feebad31ee600c0efe0bfa8
TornadoP <mathbssilva@gmail.com>
2026-05-18 23:11:27 -0300

    trocando a cor dos quadrados para branco

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Page() {
+export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-white">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -14,11 +14,11 @@ export default function Page() {
         />
 
         <div className="flex gap-6">
-          <div className="h-40 w-40 rounded-lg border border-zinc-300 bg-dark"></div>
+          <div className="h-40 w-40 rounded-lg border border-zinc-300 bg-white"></div>
 
-          <div className="h-40 w-40 rounded-lg border border-zinc-300 bg-dark"></div>
+          <div className="h-40 w-40 rounded-lg border border-zinc-300 bg-white"></div>
 
-          <div className="h-40 w-40 rounded-lg border border-zinc-300 bg-dark"></div>
+          <div className="h-40 w-40 rounded-lg border border-zinc-300 bg-white"></div>
         </div>
 
         <div className="flex-1">
