ae9ef42e78deb9289eacc444c764752681f9aa59
TornadoP <mathbssilva@gmail.com>
2026-05-21 18:38:41 -0300

    direcionando para direita

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-gradient-to-b from-[#E3F1F1] to-[#CBDfde] font-sans p-6">
       <main className="flex min-h-[650px] w-full max-w-5xl flex-col items-center justify-between rounded-3xl bg-gradient-to-b from-[#F3EAE2] to-[#E0D1D4] px-16 py-10 shadow-2xl sm:items-start">
         {/* Aqui é o código para os quadrados de teste */}
-        <div className="rounded-3xl border border-white/20 bg-white/10 p-6 shadow-xl">
+        <div className="rounded-3xl border border-white/20 bg-white/10 p-6 shadow-xl ml-auto">
           <div className="flex  flex-col gap-6 ml-auto">
             <div className="h-50 w-50 rounded-lg border border-zinc-300 bg-white"></div>
             <div className="h-50 w-50 rounded-lg border border-zinc-300 bg-white"></div>
