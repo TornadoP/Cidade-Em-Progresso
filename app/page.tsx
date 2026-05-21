bcce143bb3009b876cdadd2296349464c2c6f1f6
TornadoP <mathbssilva@gmail.com>
2026-05-21 17:07:35 -0300

    reatualizando de 350px para 650px

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-100 font-sans p-6">
-      <main className="flex min-h-[350px] w-full max-w-5xl flex-col items-center justify-between rounded-3xl bg-black px-16 py-10 shadow-2xl sm:items-start">
+      <main className="flex min-h-[650px] w-full max-w-5xl flex-col items-center justify-between rounded-3xl bg-black px-16 py-10 shadow-2xl sm:items-start">
         {/* Aqui é o código para os quadrados de teste */}
         <div className="flex gap-6">
           <div className="h-40 w-40 rounded-lg border border-zinc-300 bg-white"></div>
