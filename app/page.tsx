257fe6d66f4a152791239456658ae4d1581825b3
TornadoP <mathbssilva@gmail.com>
2026-05-22 09:26:19 -0300

    movendo card vertical dos quadrados para dentro da section

--- app/page.tsx
+++ app/page.tsx
@@ -96,17 +96,16 @@ export default function Home() {
               </div>
             </div>
           </div>
-
+          {/* Quadrados Verticais */}
+          <div className="my-8 mr-8 self-start rounded-3xl border border-white/10 bg-[#4B3348]/35 p-5 shadow-xl">
+            <div className="flex  flex-col gap-6 ">
+              <div className="h-50 w-50 rounded-lg border border-zinc-300 bg-white"></div>
+              <div className="h-50 w-50 rounded-lg border border-zinc-300 bg-white"></div>
+              <div className="h-50 w-50 rounded-lg border border-zinc-300 bg-white"></div>
+            </div>
+          </div>
           {/* Daqui para baixo fica o resto do seu conteúdo */}
         </section>
-        {/* Quadrados Verticais */}
-        <div className="my-8 mr-8 self-start rounded-3xl border border-white/10 bg-[#4B3348]/35 p-5 shadow-xl">
-          <div className="flex  flex-col gap-6 ">
-            <div className="h-50 w-50 rounded-lg border border-zinc-300 bg-white"></div>
-            <div className="h-50 w-50 rounded-lg border border-zinc-300 bg-white"></div>
-            <div className="h-50 w-50 rounded-lg border border-zinc-300 bg-white"></div>
-          </div>
-        </div>
         ;
       </main>
     </div>
