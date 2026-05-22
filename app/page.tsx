e50f064fb1a75b513a033a4a42d48cddd094fe72
TornadoP <mathbssilva@gmail.com>
2026-05-22 09:48:57 -0300

    alterando formato de dimensionamento do comprimento dos quadrados para w-[]

--- app/page.tsx
+++ app/page.tsx
@@ -97,9 +97,9 @@ export default function Home() {
             </div>
             {/* Quadrados Verticais */}
             <div className="flex w-[190px] flex-col justify-between rounded-3xl border border-white/10 bg-[#4B3348]/35 p-5 shadow-xl">
-              <div className="h-[180px] w-full rounded-lg border border-zinc-300 bg-white"></div>
-              <div className="h-[180px] w-full rounded-lg border border-zinc-300 bg-white"></div>
-              <div className="h-[180px] w-full rounded-lg border border-zinc-300 bg-white"></div>
+              <div className="h-[180px] h-[180px] rounded-lg border border-zinc-300 bg-white"></div>
+              <div className="h-[180px] h-[180px] rounded-lg border border-zinc-300 bg-white"></div>
+              <div className="h-[180px] h-[180px] rounded-lg border border-zinc-300 bg-white"></div>
             </div>
           </div>
 
