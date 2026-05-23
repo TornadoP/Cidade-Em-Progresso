58ad2023ec3908dcf81ed515e0e16ebaa5c9b366
TornadoP <mathbssilva@gmail.com>
2026-05-22 22:03:01 -0300

    link é errado tem que escrever Link

--- app/page.tsx
+++ app/page.tsx
@@ -42,12 +42,12 @@ export default function Home() {
             >
               Inicio
             </a>
-            <link
+            <Link
               className="rounded-xl px-4 py-3 transition hover:bg-[#FFC222]"
               href="/obras"
             >
               Obras
-            </link>
+            </Link>
             <a
               className="rounded-xl px-4 py-3 transition hover:bg-[#FFC222]"
               href="#"
