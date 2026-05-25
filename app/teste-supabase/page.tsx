9be153fa4e6dd2344cec4514dd3da31ff9f387ec
TornadoP <mathbssilva@gmail.com>
2026-05-25 11:06:04 -0300

    fazendo um teste com o banco de dados supabase

---
+++ app/teste-supabase/page.tsx
@@ -0,0 +1,22 @@
+import { supabase } from "@/app/lib/supabaseClient";
+
+export default async function TesteSupabasePage() {
+  const { data: obras, error } = await supabase.from("obras").select("*");
+
+  if (error) {
+    return (
+      <div className="p-10">
+        <h1>Erro ao buscar obras</h1>
+        <pre>{error.message}</pre>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-10">
+      <h1 className="mb-4 text-2xl font-bold">Teste Supabase</h1>
+
+      <pre>{JSON.stringify(obras, null, 2)}</pre>
+    </div>
+  );
+}
